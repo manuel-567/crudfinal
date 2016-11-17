6b8cc554a7c064fbe04b65bd5640700e2b647f08
manueledu7 <manuel.567@hotmail.com>
2016-11-17 16:13:29 -0600

    El uso de otro archivo url para no saturar el que trae por defecto django y la creacion de un form

--- crudfinal/views.py
+++ crudfinal/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response,render
 
-def home(request):
-    return render_to_response('index.html')
+#def home(request):
+#    return render_to_response('index.html')
+def post_list(request):
+    return render(request, 'crudfinal/post_list.html', {})
