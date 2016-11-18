6c74825b617fed5dd9f154df0afd15c90b411450
manueledu7 <manuel.567@hotmail.com>
2016-11-18 07:41:58 -0600

    Detalle de las publicaciones

--- crudfinal/urls.py
+++ crudfinal/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^crudfinal/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
-
 
 ]
--- crudfinal/views.py
+++ crudfinal/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render_to_response,render
+from django.shortcuts import render_to_response,render,get_object_or_404
 from django.utils import timezone
 from .models import Foto
+
 
 
 def post_list(request):
@@ -10,3 +11,6 @@ def post_list(request):
 #    return render_to_response('index.html')
 #def post_list(request):
 #    return render(request, 'crudfinal/post_list.html', {})
+def post_detail(request, pk):
+        post = get_object_or_404(Foto, pk=pk)
+        return render(request, 'crudfinal/post_detail.html', {'post': post})
